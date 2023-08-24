40d3cc04d4e4ab3cbd52e9ab1a0de500997f20f0
JLugo4 <jarek.lugo96@gmail.com>
2023-08-23 20:49:47 -0400

    Part 1.1 done

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,25 @@
 package com.github.curriculeon;
 
 public class Person {
+    final long id;
+    String name;
 
+    public Person(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    // GETTERS AND SETTERS
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    // END. GETTERS AND SETTERS
 }
--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,23 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    // Remember GIVEN, WHEN, THEN
+
+    @Test
+    public void testPerson1() {
+        //Given
+        long expectedId = 12345;
+        String expectedName = "Dean";
+
+        //When
+        Person person = new Person(expectedId, expectedName);
+
+        //Then
+        Assert.assertEquals(expectedId, person.getId());
+        Assert.assertEquals(expectedName, person.getName());
+    }
 }
