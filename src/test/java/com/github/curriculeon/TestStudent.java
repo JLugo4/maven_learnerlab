f96e65d144aa7e61d68bc1abb538d58982391fe9
JLugo4 <jarek.lugo96@gmail.com>
2023-08-26 20:35:01 -0400

    Learn test is trash

--- src/test/java/com/github/curriculeon/TestStudent.java
+++ src/test/java/com/github/curriculeon/TestStudent.java
@@ -1,6 +1,9 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestStudent {
 
@@ -15,6 +18,31 @@ public class TestStudent {
 
 
         //Then
+        Assert.assertTrue(student instanceof Learner);
+    }
+    @Test
+    public void testIsPerson(){
+        //Given
+        Student student = new Student(10000, "Dean");
 
+        //When
+
+
+        //Then
+        Assert.assertTrue(student instanceof Person);
+    }
+
+    @Test
+    public void testLearn(){
+        //Given
+        Student student = new Student(10000, "Dean");
+
+        //When
+        student.learn(10.0);
+        student.learn(8.0);
+
+        //Then
+        double totalStudyTime = student.getTotalStudyTime();
+        Assert.assertEquals(18.0, totalStudyTime);
     }
 }
