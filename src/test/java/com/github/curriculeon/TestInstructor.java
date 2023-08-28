1b8e87596946169f902ed56c2676dda60ac23cf2
JLugo4 <jarek.lugo96@gmail.com>
2023-08-28 00:00:43 -0400

    Working on Instructor Tests

--- src/test/java/com/github/curriculeon/TestInstructor.java
+++ src/test/java/com/github/curriculeon/TestInstructor.java
@@ -1,5 +1,51 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestInstructor {
 
+    @Test
+    public void testIsInstructorIsTeacher(){
+        //Given
+        Instructor instructor = new Instructor(10000, "Leon");
+
+        //When
+
+
+        //Then
+        Assert.assertTrue(instructor instanceof Teacher);
+    }
+
+    @Test
+    public void testIsInstructorIsPerson(){
+        //Given
+        Instructor instructor = new Instructor(10001, "Noel");
+
+        //When
+
+
+        //Then
+        Assert.assertTrue(instructor instanceof Person);
+    }
+
+    @Test
+    public void testTeachInInstructor(){
+        // Given
+
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    public void testLectureInInstructor(){
+        // Given
+
+        //When
+
+        //Then
+
+    }
 }
